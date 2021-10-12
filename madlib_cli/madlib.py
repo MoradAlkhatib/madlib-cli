3ffb3b8d6b5f1d3f4e42824b1222fcb7af8664c4
moradalkhatib <muradalkhateb@icloud.com>
2021-10-12 19:48:21 +0300

    first Function that take the path as parameter and return what the file have then i make a test for that

---
+++ madlib_cli/madlib.py
@@ -0,0 +1,8 @@
+print("Welcome You are in the Madlib Game")
+
+def read_template(path):
+    """ function how read file from the url
+        and return what that file have 
+    """ 
+    a=open(path)
+    return a.read()
